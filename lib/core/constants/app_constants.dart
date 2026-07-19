b832c40a66b3baf90d37998404c4aa5909e70cd4
DueTonight <duetonightapp@gmail.com>
2026-07-19 22:33:13 +0530

    Backend update

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -4,7 +4,11 @@ class AppConstants {
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRkb3RuZHJhcGZhd2dsanl6cmtuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg3NzYzNTQsImV4cCI6MjA5NDM1MjM1NH0.eGmy70EUcoAtqixypn6_eZMvGziXtNmLTj3sPbsYNQU';
   static const String googleClientId =
       '63754572308-pn8uoavsl9sd50g76saqi3n6qtr3cdei.apps.googleusercontent.com';
+  // For Android emulator, use http://10.0.2.2:3000
+  // For web (local dev), update to http://localhost:3000
+  // For production, set to the actual deployed backend URL (e.g. https://api.duetonight.app)
   static const String backendUrl = 'http://10.0.2.2:3000';
+  static const String backendUrlWeb = 'http://localhost:3000';
   static const String cloudinaryCloudName = 'dbzl3odk6';
   static const String cloudinaryUploadPreset = 'duetonight_uploads';
   static const String vapidPublicKey =
